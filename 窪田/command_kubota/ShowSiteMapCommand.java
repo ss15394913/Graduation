9e9c2b8b86bad84f88b74f10f75e570de8b9d548
harukikubota <hk15304009@ga.tera-house.ac.jp>
2017-02-07 11:57:30 +0900

    ただ飛ばすマン
    「私の策を信じろ」

---
+++ 窪田/command_kubota/ShowSiteMapCommand.java
@@ -0,0 +1,23 @@
+﻿/*
+  @author 窪田遥月
+  @date 2017/02/07
+*/
+
+package command;
+
+import logic.RequestContext;
+import logic.ResponseContext;
+import logic.WebRequestContext;
+
+import ex.LogicException;
+
+/* ただ飛ばすマンのひな形コマンド */
+public class ShowSiteMapCommand extends AbstractCommand {
+	public ResponseContext execute(ResponseContext responseContext)
+	throws LogicException{
+		
+		responseContext.setTarget("sitemap");
+		
+		return responseContext;
+	}
+}
